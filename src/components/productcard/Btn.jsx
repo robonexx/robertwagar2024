1416da6e304d5438ea0f4cc7c331bd869531921f
robonexx <robertwagar@gmail.com>
2023-11-13 15:12:43 +0100

    need changes

--- src/components/productcard/Btn.jsx
+++ src/components/productcard/Btn.jsx
@@ -1,28 +1,25 @@
 const getVariant = (variant) => {
   switch (variant) {
     case 'primary':
-      return 'bg-indigo-500 hover:bg-indigo-700  text-white shadow shadow-blue-600/25 hover:shadow-blue-600/75';
+      return styled.button`
+        ${baseStyles}
+        background-color: #6366f1;
+        color: #fff;
+        box-shadow: 0 2px 4px rgba(66, 153, 225, 0.25), 0 8px 16px rgba(66, 153, 225, 0.75);
+        &:hover {
+          box-shadow: 0 2px 8px rgba(66, 153, 225, 0.5), 0 12px 24px rgba(66, 153, 225, 0.9);
+        }
+      `;
     case 'danger':
-      return 'bg-red-500 hover:bg-red-700 text-white  shadow shadow-red-600/25 hover:shadow-red-600/75';
-    case 'success':
-      return 'bg-green-500 hover:bg-green-700 text-white shadow shadow-green-600/25 hover:shadow-green-600/75 ';
-    case 'warning':
-      return 'bg-amber-500 hover:bg-amber-700 text-white shadow shadow-yellow-600/25 hover:shadow-yellow-600/75 ';
-    case 'outline-danger':
-      return 'bg-white text-red-500 border border-red-500 hover:text-white hover:bg-red-700  ';
-    case 'outline-danger':
-      return 'bg-white text-red-500 border border-red-500 hover:text-white hover:bg-red-700  ';
-    case 'outline-success':
-      return 'bg-white text-green-500 border border-green-500 hover:text-white hover:bg-green-700  ';
-    case 'outline-warning':
-      return 'bg-white text-amber-400 border border-amber-500 hover:text-white hover:bg-amber-500  ';
-    case 'outline-primary':
-      return 'bg-white text-indigo-500 border border-violet-500 hover:text-white hover:bg-indigo-700  ';
-
-    default:
-      return 'bg-indigo-500 hover:bg-indigo-700 text-white shadow shadow-indigo-600/25 hover:shadow-indigo-600/75';
-  }
-};
+      return styled.button`
+        ${baseStyles}
+        background-color: #ef4444;
+        color: #fff;
+        box-shadow: 0 2px 4px rgba(239, 68, 68, 0.25), 0 8px 16px rgba(239, 68, 68, 0.75);
+        &:hover {
+          box-shadow: 0 2px 8px rgba(239, 68, 68, 0.5), 0 12px 24px rgba(239, 68, 68, 0.9);
+        }
+      `;
 
 export const Btn = ({
   className,
@@ -37,7 +34,7 @@ export const Btn = ({
     <button
       {...props}
       type={type}
-      varint={variant}
+      variant={getVariant(variant)}
      /*  className={` */
      /*      ${getVariant(variant)}  transition duration-300  ${ */
      /*    !paddingLess && 'p-1' */
