e822d71b5f633e44ac213780f330e4330dda0561
robonexx <robertwagar@gmail.com>
2023-11-25 11:29:08 +0100

    ... vite env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'process.env': process.env,
+    VITE_SENDER_ID: process.env.VITE_SENDER_ID,
+    VITE_TEMP_ID: process.env.VITE_TEMP_ID,
+    VITE_KEY: process.env.VITE_KEY,
+    VITE_PRIVATE: process.env.VITE_PRIVAT,
+  },
+});
